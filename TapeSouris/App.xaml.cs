9193b70fca1a3b93ebe013c83885d2bcd3379f8c
Gabriel_BUJOR <gabriel.bujor@etu.univ-smb.fr>
2025-12-09 10:16:00 +0100

    Ajoutez des fichiers projet.

---
+++ TapeSouris/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TapeSouris
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
